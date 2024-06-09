78c182ddcf390d32c1a489549b12d7e810ec6e55
prosper williams <wprosper92@gmail.com>
2024-06-09 18:26:24 +0100

    updated my files, installed neccesary dependencies and ready to begin!

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+import MainLayout from "./Components/Layout/MainLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <BrowserRouter>
+         <Routes>
+            <Route element={<MainLayout />}>
+               <Route path={"/"} element={<Home />} />
+            </Route>
+         </Routes>
+      </BrowserRouter>
+   </React.StrictMode>
+);
